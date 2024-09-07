eddd60cd3bbb8f5d432c88aa9db88b8cee9f88c3
meicale <bill@DESKTOP-M0LLBM9.>
2024-09-07 22:55:30 +0800

    update home.nix adding neovim

--- wsl/home-manager/home.nix
+++ wsl/home-manager/home.nix
@@ -50,7 +50,8 @@
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+  programs.neovim.enable = true;
+  # programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
